731dad081f4a592bab32391800897fc95fbb6cd7
sgewanter <122758088+sgewanter@users.noreply.github.com>
2023-11-07 16:47:49 -0500

    Worked in the Client and Master classes

--- Client.java
+++ Client.java
@@ -1,5 +1,10 @@
 package yg;
 
 public class Client {
-
+    private Type type;
+    private int ID;
+    public Client(Type type, int ID){
+        this.type = type;
+        this.ID = ID;
+    }
 }
--- Master.java
+++ Master.java
@@ -1,5 +1,7 @@
 package yg;
 
 public class Master {
+    private Slave slaveA;
+    private Slave slaveB;
 
 }
