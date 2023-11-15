e3055256fabc8a68d4dfd3d0129d8bd6f367c991
brodkin-3 <121911359+brodkin-3@users.noreply.github.com>
2023-11-15 16:08:53 -0500

    I worked on the client and made it a socket

--- Client.java
+++ Client.java
@@ -1,10 +1,64 @@
 package yg;
 
+/*
+ob class, have an arraylist of jobs loaded up and it will send them one at a time to the master
+For now, we’ll hardcode the jobs. The client will take those, a thread will send it to the master. We need one main
+*/
+
+import java.io.*;
+import java.net.*;
+
 public class Client {
-    private Type type;
+
+    public static void main(String args[]) {
+        // this is going to be a temporary list to hold jobs
+
+        // these args are hardcoded, but we can enter them if we want into the command line
+        // possible option to have an arg to create two instances of this?
+        args = new String[]{"127.0.0.1", "30121"};
+
+        if (args.length != 2) {
+            System.err.println("Usage: java client <host name> <port number>");
+            System.exit(1);
+        }
+
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        try (
+                Socket clientSocket = new Socket(hostName, portNumber);
+                PrintWriter requestWriter = //stream to write text requests to server
+                        new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader infoReader = //stream to read response from server
+                        new BufferedReader(
+                                new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+            // this is going to be the code where we send things to the master
+            String info = infoReader.readLine();
+            // this will be reading from the socket
+            while(info != null) {
+                // this will be checking if master says that the job is finished
+                // then it will send another one
+                if(info == "done") {
+                    getNewJob();
+                }
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " + hostName);
+            System.exit(1);
+        }
+    /*private Type type;
     private int ID;
     public Client(Type type, int ID){
         this.type = type;
         this.ID = ID;
+    }*/
+    }
+
+    public static void getNewJob() {
+
     }
 }
--- Master.java
+++ Master.java
@@ -4,4 +4,5 @@ public class Master {
     private Slave slaveA;
     private Slave slaveB;
 
+
 }
