d5464115472507639af7cf3437cee71d5d7c4e36
brodkin-3 <121911359+brodkin-3@users.noreply.github.com>
2024-01-16 20:31:39 -0500

    Update SlaveB.java

--- Slave/SlaveB.java
+++ Slave/SlaveB.java
@@ -49,6 +49,8 @@ public class SlaveB {
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostName);
             System.exit(1);
+        } catch (EOFException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             System.err.println("Couldn't get I/O for the connection to " + hostName);
             e.printStackTrace();
